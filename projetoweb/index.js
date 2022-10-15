471936faacbdcfae275adda44f18150cf7d3b65a
rrAlfaro <rafael@alfaro.com.br>
2022-10-14 21:19:52 -0300

    lala

--- projetoweb/index.js
+++ projetoweb/index.js
@@ -7,9 +7,28 @@ const path = require('path');
 app.set('view engine', 'handlebars');
 app.engine('handlebars', engine());
 
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+const fakedata = [
+    {
+        id: 1,
+        nome: 'Zezinho',
+        endereco: 'Rua lalala 100',
+        sexo: 'Masculino',
+        telefone: '5555-1234'
+    },
+    {
+        id: 2,
+        nome: 'Mariazinha',
+        endereco: 'Rua lululu 200',
+        sexo: 'Feminino',
+        telefone: '5555-4321'
+    }
+];
 
 app.get("/", function(req,res){
     // res.send(`Eu não acredito ${req.query['nome']} ${req.query['sobrenome']}`);
